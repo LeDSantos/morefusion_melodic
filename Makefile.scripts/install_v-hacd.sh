78d95e6c12bf45c763d52e9897d622054df774ae
Letícia dos Santos <d.santos.leti@gmail.com>
2023-03-08 17:21:58 +0100

    git checkout to solve morefusion_melodic/src/v-hacd/src does not exist

--- Makefile.scripts/install_v-hacd.sh
+++ Makefile.scripts/install_v-hacd.sh
@@ -14,6 +14,10 @@ fi
 
 if [ ! -e $ROOT/.anaconda3/bin/testVHACD ]; then
   cd $ROOT/src/v-hacd
+  # https://github.com/wkentaro/morefusion/issues/102#issue-1270898773
+  # This is due to changes in the v-hacd repo.
+  # You can solve the problem by switching to an older version. In the morefusion/src/v-hacd directory, do:
+  git checkout 1a49edf29c69039df15286181f2f27e17ceb9aef
   mkdir -p build
   cd build
   cmake ../src
